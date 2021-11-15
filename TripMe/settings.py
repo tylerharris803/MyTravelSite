6c1b3c9412d619e1c7e70f32e8865b679eb47917
tylerharris803 <tylerharris803@gmail.com>
2021-11-15 16:02:38 -0700

    model and admin commit

--- TripMe/settings.py
+++ TripMe/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'TripMe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'registration',
+        'USER' : 'postgres' ,
+        'PASSWORD' : 'jimmertime',
+        'HOST' : 'localhost'
     }
 }
 
